c1ec8f496c12be1312a7c5a730a8672a5be57f2a
Ronke <adepojuronkemercy@gmail.com>
2021-07-17 15:31:55 +0100

    score

---
+++ database/migrations/2021_07_17_141325_create_score_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScoreTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('score', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('school_id');
+            $table->foreignId('subject_id');
+            $table->foreignId('student_id');
+            $table->unsignedInterger('score');
+            $table->date('created_at');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('score');
+    }
+}
