93dcd47431635cb673cec6d9b68ffe4c7217a56a
AlphaChris14 <86351720+AlphaChris14@users.noreply.github.com>
2021-08-02 19:25:02 +0100

    Update 2021_07_28_041717_create_scores_table.php

--- database/migrations/2021_07_28_041717_create_scores_table.php
+++ database/migrations/2021_07_28_041717_create_scores_table.php
@@ -17,8 +17,8 @@ class CreateScoresTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('utme_reg_no')->unique();
-            $table->string('course')->nullable();
-            $table->string('score')->nullable();
+            $table->string('course');
+            $table->string('score');
             $table->string('user_image');
             $table->timestamps();
         });
